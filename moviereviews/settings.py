77f8bfaa5d57bc5adb9b6a140698fe0af1101cc3
GianSz <giansz2003@gmail.com>
2022-11-01 09:49:42 -0500

    Nueva IP

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-0952@dh1pfu@kf7id-**$x5-ktr-4v_^^(!xs&uwljy_ywyasp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.28.94.248']
 
 
 # Application definition
